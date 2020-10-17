06b339c9f354676465aa114f79b3416c3db1f45c
DyingA <971258403@qq.com>
2020-10-17 12:12:45 +0800

    work is ok

---
+++ work/precache-manifest.3b8e6c01bfda73eed10c464420e01982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18560a171c6de2ecf4001010f00e4f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "897c63d9fdc30ad5f6b8",
+    "url": "/static/css/main.ebdf2b15.chunk.css"
+  },
+  {
+    "revision": "d84114bef6815034c19f",
+    "url": "/static/js/2.7571a8f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7571a8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897c63d9fdc30ad5f6b8",
+    "url": "/static/js/main.8bf83a3a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
